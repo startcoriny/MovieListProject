2b3da979656ca881fa55848d3b27974feaf199e7
startcoriny <127919222+startcoriny@users.noreply.github.com>
2024-01-06 16:51:37 +0900

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-const config = {
-    API_KEY: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNTEwZDdmNWYzMmRlMGQ1ZGJmYmZhYTQzYzU1MTlmMyIsInN1YiI6IjY1OTYzYTkxMGU2NGFmNDAxMThjMTkxYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QB8-UFSN1x6St9D-218WxnUeZVQFaw9-h8zZXbF1pcM",
-  };
-  export default config;
